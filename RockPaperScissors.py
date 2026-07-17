c4cd740414acf7ec63bdd528eb2f398385341c0d
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-17 23:27:00 +0530

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,1 +1,12 @@
-
+from tkinter import *
+from PIL import Image, ImageTk
+import random
+import sqlite3
+root = Tk()
+root.title('Rock Paper Scissors Game')
+root.resizable(width=False, height=False)
+click = True
+userWin = 0
+compWin = 0
+counter = 0
+turns = 5
