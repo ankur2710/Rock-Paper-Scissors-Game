9268bf162a5c86bc9ed69ff82058db4efccccbdd
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-17 23:29:25 +0530

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -119,3 +119,118 @@ getLeaderboard()
     resetButton.pack(pady = 5)
     LeaderBoardBtn = Button(buttonHolder, text = 'Leader Board',fg = 'white', command=lambda: reset_frame(), bg = 'black', width = 30, pady = 10)
     LeaderBoardBtn.pack(pady = 5)
+def computerPick():
+    choice = random.choice(['rock','paper','scissors'])
+    return choice
+def youPick(yourChoice):
+    global click, userWin, compWin, Scoreboard, counter, turns
+    compPick = computerPick()
+    if click:
+        counter += 1
+        if yourChoice == 'rock':
+            rHandButton.configure(image = rockImage)
+            rockLabel.configure(text = 'Rock')
+            if compPick == 'rock':
+                pHandButton.configure(image = rockImage)
+                sHandButton.configure(image = tieImage)
+                paperLabel.configure(text='Rock')
+                scissorLabel.configure(text='Tie')
+                click = False
+            elif compPick == 'paper':
+                pHandButton.configure(image = paperImage)
+                sHandButton.configure(image = loseImage)
+                paperLabel.configure(text='Paper')
+                scissorLabel.configure(text='Lose')
+                compWin+=1
+                click = False
+            else :
+                pHandButton.configure(image = scissorImage)
+                sHandButton.configure(image = winImage)
+                paperLabel.configure(text='Scissors')
+                scissorLabel.configure(text='Win')
+                userWin+=1
+                click = False
+        elif yourChoice == 'paper':
+            rHandButton.configure(image=paperImage)
+            rockLabel.configure(text='Paper')
+            if compPick == 'rock':
+                pHandButton.configure(image=rockImage)
+                sHandButton.configure(image=winImage)
+                paperLabel.configure(text='Rock')
+                scissorLabel.configure(text='Win')
+                userWin += 1
+                click = False
+            elif compPick == 'paper':
+                pHandButton.configure(image=paperImage)
+                sHandButton.configure(image=tieImage)
+                paperLabel.configure(text='Paper')
+                scissorLabel.configure(text='Tie')
+                click = False
+            else:
+                pHandButton.configure(image=scissorImage)
+                sHandButton.configure(image=loseImage)
+                paperLabel.configure(text='Scissors')
+                scissorLabel.configure(text='Lose')
+                compWin += 1
+                click = False
+        else:
+            rHandButton.configure(image=scissorImage)
+            rockLabel.configure(text='Scissors')
+            if compPick == 'rock':
+                pHandButton.configure(image=rockImage)
+                sHandButton.configure(image=loseImage)
+                paperLabel.configure(text='Rock')
+                scissorLabel.configure(text='Lose')
+                compWin += 1
+                click = False
+            elif compPick == 'paper':
+                pHandButton.configure(image=paperImage)
+                sHandButton.configure(image=winImage)
+                paperLabel.configure(text='Paper')
+                scissorLabel.configure(text='Win')
+                userWin += 1
+                click = False
+            else:
+                pHandButton.configure(image=scissorImage)
+                sHandButton.configure(image=tieImage)
+                paperLabel.configure(text='Scissors')
+                scissorLabel.configure(text='Tie')
+                click = False
+    else:
+        if yourChoice == 'rock' or yourChoice == 'paper' or yourChoice == 'scissors':
+            rHandButton.configure(image = rHandPhoto)
+            pHandButton.configure(image = pHandPhoto)
+            sHandButton.configure(image = sHandPhoto)
+            rockLabel.configure(text='Rock')
+            paperLabel.configure(text='Paper')
+            scissorLabel.configure(text='Scissors')
+            click = True
+  Scoreboard = Label(root, text="SCORE \n\n    USER - " + str(userWin) + "\t\tCOMPUTER - " + str(compWin), bg='blue',
+                       fg='white', padx=10, pady=20)
+    Scoreboard.config(font=("Times", 15))
+    Scoreboard.grid(row=2, column=0, columnspan=2, sticky=W + E, padx=10, pady=10)
+    if counter == turns:
+        message = ''
+        if userWin>compWin:
+            message = 'You Won!!'
+        elif userWin<compWin:
+            message = 'You Lose!!'
+        else:
+            message = 'You Tied!!'
+        top = Toplevel()
+        top.geometry('300x300')
+        confetiImg = PhotoImage(file = "RockPaperScissorsImages/confeti.gif")
+        confetiLabel = Label(top, image = confetiImg)
+        confetiLabel.image = confetiImg
+        confetiLabel.grid(row = 0, column = 0)
+        messageFrame = Frame(top)
+        messageFrame.grid(row = 0, column=0)
+        message = Label(messageFrame, text = message)
+        message.config(font=("Times", 30, 'bold'))
+        message.pack()
+        exitButton = Button(messageFrame, text = "Exit", bg = 'red', fg = 'white', width = 10, padx = 5, pady = 5, command = root.quit)
+        exitButton.config(font=("Times", 12))
+        exitButton.pack(pady = 5, padx = 5)
+        rHandButton.configure(state = "disabled")
+        pHandButton.configure(state="disabled")
+        sHandButton.configure(state="disabled")
