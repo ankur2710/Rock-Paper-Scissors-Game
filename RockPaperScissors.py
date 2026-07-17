e7f7a9bda7921c0eeb2c26b61232cf0034e4cce3
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-17 23:27:33 +0530

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -10,3 +10,20 @@ userWin = 0
 compWin = 0
 counter = 0
 turns = 5
+#----------------Image set----------------
+rHandPhoto = PhotoImage(file = 'rHand.png')
+pHandPhoto = PhotoImage(file = 'pHand.png')
+sHandPhoto = PhotoImage(file = 'sHand.png')
+rock = Image.open("RockPaperScissorsImages/Rockimg.jpg")
+rockImage = ImageTk.PhotoImage(rock)
+paper = Image.open("RockPaperScissorsImages/Paperimg.jpg")
+paperImage = ImageTk.PhotoImage(paper)
+scissors = Image.open("RockPaperScissorsImages/Scissorsimg.jpg")
+scissorImage = ImageTk.PhotoImage(scissors)
+win = Image.open("RockPaperScissorsImages/YouWin.jpg")
+winImage = ImageTk.PhotoImage(win)
+lose = Image.open("RockPaperScissorsImages/YouLose.jpg")
+loseImage = ImageTk.PhotoImage(lose)
+tie = Image.open("RockPaperScissorsImages/YouTie.jpg")
+tieImage = ImageTk.PhotoImage(tie)
+#------------------------------------------
